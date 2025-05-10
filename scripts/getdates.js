d51d60cd7ab794a9a63241e144f5261ab19d6ac5
gracie12305 <kwofib5@gmail.com>
2025-05-10 17:14:14 +0200

    changes

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+//  To get the current y
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+//To set last modified
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
